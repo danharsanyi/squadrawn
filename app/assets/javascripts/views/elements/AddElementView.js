f60d83a9c6fcfa03ce8c04bf0fcf4fc82e0e8c67
doloreel <jarimado@gmail.com>
2016-06-21 10:28:31 +1000

    destroying all messages on rake db:seed

---
+++ app/assets/javascripts/views/elements/AddElementView.js
@@ -0,0 +1,17 @@
+var app = app || {};
+
+app.AddElementView = Backbone.View.extend({
+    el: "#elementsList",
+    render: function() {
+        console.log('element list view initialised');
+        var elementListTemplate = $("#elementListTemplate").html();
+        this.$el.html(elementListTemplate);
+
+        var currentElements = app.elements.toJSON();
+
+        currentElements.forEach(function (element) {
+            var currentElementItem = new app.ElementView();
+            currentElementItem.render(element);
+        });
+    }
+});
