f439eb3f707a111dac91f1aaa14f143ff7a81231
doloreel <jarimado@gmail.com>
2016-06-23 09:28:04 +1000

    added comments

--- app/assets/javascripts/views/checkout/CheckOutPaymentView.js
+++ app/assets/javascripts/views/checkout/CheckOutPaymentView.js
@@ -9,6 +9,16 @@ app.CheckOutPaymentView = Backbone.View.extend({
 
         this.$el.find('')
 
+        // app.util.key --------------------------------------------------------
+        // this has the publishable key from rails
+        // we save it in the application.html.erb :
+        // // <script>
+        // //         var app = app || {};
+        // //         app.util = {};
+        // //         app.util.key = "<%= Rails.configuration.stripe[:publishable_key] %>";
+        // // </script>
+        // ---------------------------------------------------------------------
+
         var stripeButtonTemplate = '<script src="https://checkout.stripe.com/checkout.js" class="stripe-button" data-key="' + app.util.key + '" data-description="Launch Survey" data-amount="<%= @amount %>"></script>';
         this.$el.find('form').append(stripeButtonTemplate);
     }
