7e06b411184ceacd62234fae595b2ad8e8dade03
doloreel <jarimado@gmail.com>
2016-06-22 10:43:25 +1000

    added different id color for different element types

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,20 +25,20 @@ Element.create :element_type => "image", :element_data => '{"type":"IMAGE","name
 
 Element.create :element_type => "image", :element_data => '{"type":"IMAGE","name":"Cube Maze","id":3,"value":{"url":"http://bashooka.com/wp-content/uploads/2014/06/geometry-design-inspiration-8.jpg","dimensions":{"w":400,"h":300}}}'
 
-Element.create :element_type => "image", :element_data => '{"type":"IMAGE","name":"Box Ribbon","id":3,"value":{"url":"https://colorlib.com/wp/wp-content/uploads/sites/2/2013/10/BoldMedia-flat-logo.png","dimensions":{"w":400,"h":300}}}'
+Element.create :element_type => "text", :element_data => '{"type":"IMAGE","name":"Box Ribbon","id":3,"value":{"url":"https://colorlib.com/wp/wp-content/uploads/sites/2/2013/10/BoldMedia-flat-logo.png","dimensions":{"w":400,"h":300}}}'
+
+Element.create :element_type => "image", :element_data => '{"type":"IMAGE","name":"Fox Color","id":3,"value":{"url":"http://www.faceliftdesigns.com/clientuploads/directory/2012_responsive_homeboxes/design.png","dimensions":{"w":400,"h":300}}}'
+
+Element.create :element_type => "shape", :element_data => '{"type":"IMAGE","name":"Cube Maze","id":3,"value":{"url":"http://bashooka.com/wp-content/uploads/2014/06/geometry-design-inspiration-8.jpg","dimensions":{"w":400,"h":300}}}'
+
+Element.create :element_type => "shape", :element_data => '{"type":"IMAGE","name":"Box Ribbon","id":3,"value":{"url":"https://colorlib.com/wp/wp-content/uploads/sites/2/2013/10/BoldMedia-flat-logo.png","dimensions":{"w":400,"h":300}}}'
 
 Element.create :element_type => "image", :element_data => '{"type":"IMAGE","name":"Fox Color","id":3,"value":{"url":"http://www.faceliftdesigns.com/clientuploads/directory/2012_responsive_homeboxes/design.png","dimensions":{"w":400,"h":300}}}'
 
 Element.create :element_type => "image", :element_data => '{"type":"IMAGE","name":"Cube Maze","id":3,"value":{"url":"http://bashooka.com/wp-content/uploads/2014/06/geometry-design-inspiration-8.jpg","dimensions":{"w":400,"h":300}}}'
 
-Element.create :element_type => "image", :element_data => '{"type":"IMAGE","name":"Box Ribbon","id":3,"value":{"url":"https://colorlib.com/wp/wp-content/uploads/sites/2/2013/10/BoldMedia-flat-logo.png","dimensions":{"w":400,"h":300}}}'
+Element.create :element_type => "text", :element_data => '{"type":"IMAGE","name":"Box Ribbon","id":3,"value":{"url":"https://colorlib.com/wp/wp-content/uploads/sites/2/2013/10/BoldMedia-flat-logo.png","dimensions":{"w":400,"h":300}}}'
 
-Element.create :element_type => "image", :element_data => '{"type":"IMAGE","name":"Fox Color","id":3,"value":{"url":"http://www.faceliftdesigns.com/clientuploads/directory/2012_responsive_homeboxes/design.png","dimensions":{"w":400,"h":300}}}'
+Element.create :element_type => "text", :element_data => '{"type":"IMAGE","name":"Fox Color","id":3,"value":{"url":"http://www.faceliftdesigns.com/clientuploads/directory/2012_responsive_homeboxes/design.png","dimensions":{"w":400,"h":300}}}'
 
-Element.create :element_type => "image", :element_data => '{"type":"IMAGE","name":"Cube Maze","id":3,"value":{"url":"http://bashooka.com/wp-content/uploads/2014/06/geometry-design-inspiration-8.jpg","dimensions":{"w":400,"h":300}}}'
-
-Element.create :element_type => "image", :element_data => '{"type":"IMAGE","name":"Box Ribbon","id":3,"value":{"url":"https://colorlib.com/wp/wp-content/uploads/sites/2/2013/10/BoldMedia-flat-logo.png","dimensions":{"w":400,"h":300}}}'
-
-Element.create :element_type => "image", :element_data => '{"type":"IMAGE","name":"Fox Color","id":3,"value":{"url":"http://www.faceliftdesigns.com/clientuploads/directory/2012_responsive_homeboxes/design.png","dimensions":{"w":400,"h":300}}}'
-
-Element.create :element_type => "image", :element_data => '{"type":"IMAGE","name":"Cube Maze","id":3,"value":{"url":"http://bashooka.com/wp-content/uploads/2014/06/geometry-design-inspiration-8.jpg","dimensions":{"w":400,"h":300}}}'
+Element.create :element_type => "shape", :element_data => '{"type":"IMAGE","name":"Cube Maze","id":3,"value":{"url":"http://bashooka.com/wp-content/uploads/2014/06/geometry-design-inspiration-8.jpg","dimensions":{"w":400,"h":300}}}'
