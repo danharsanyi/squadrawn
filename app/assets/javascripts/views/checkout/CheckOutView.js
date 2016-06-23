7968afc82a9d7f5cb9434f2c683ca6c812a78af0
onesc <onescyt@gmail.com>
2016-06-23 19:10:49 +1000

    fixed stripe

--- app/assets/javascripts/views/checkout/CheckOutView.js
+++ app/assets/javascripts/views/checkout/CheckOutView.js
@@ -3,11 +3,23 @@ var app = app || {};
 app.CheckOutView = Backbone.View.extend({
     el: '#main',
 
-  
+    saveDesign: function() {
+      var designID = app.designs.get(app.currentDesignID);
+      var designJSON = saveCanvas();
+      designID.set("canvas_data", designJSON);
+      designID.save();
+    },
 
-    render: function ( data ) {
+    render: function ( id ) {
         this.saveDesign();
         var checkOutViewTemplate = $('#checkOutViewTemplate').html();
         this.$el.html(checkOutViewTemplate);
+
+        var checkOutDesignView = new app.CheckOutDesignView();
+        checkOutDesignView.render( id );
+
+        var checkOutPaymentView = new app.CheckOutPaymentView();
+        checkOutPaymentView.render( );
+
     }
-})
+});
